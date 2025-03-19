1eaf0cf5597f1ed60c2e6d0b887987af10a21fd7
chara001-admin <molagon94@gmail.com>
2025-03-19 10:33:57 +0800

    進入第三章

---
+++ C#設計實務/Ch-3型別_字面字_變數/Decimal/Decimal.cs
@@ -0,0 +1,26 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learing
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            decimal price;
+            decimal discount;
+            decimal discounted_price;
+
+
+            price = 19.95m;
+            discount = 0.15m;
+
+            discounted_price = price - (price * discount);
+
+            Console.WriteLine("Discounted price: $" + discounted_price);
+        }
+    }
+}
---
+++ C#設計實務/Ch-3型別_字面字_變數/Decimal/Decimal1.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learing
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            decimal amount;
+            decimal rate_of_return;
+            int years, i;
+
+            amount = 1000.0m;
+            rate_of_return = 0.07m;
+            years = 10;
+
+            Console.WriteLine("Original investmwnt: $" + amount);
+            Console.WriteLine("Rate of return: $" + rate_of_return);
+            Console.WriteLine("Over " + years + " years");
+
+            for(i = 0; i < years; i++)
+            {
+                amount = amount + (amount * rate_of_return);
+            }
+
+            Console.WriteLine("Future value: $" + amount); //會列印完整長度的數字
+        }
+    }
+}
