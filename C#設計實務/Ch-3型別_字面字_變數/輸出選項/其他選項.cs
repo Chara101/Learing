2e11715d5b17de14bcf87ef3a8c7e928fdf28b20
chara001-admin <molagon94@gmail.com>
2025-03-21 10:56:37 +0800

    Complete chapter 3

---
+++ C#設計實務/Ch-3型別_字面字_變數/輸出選項/其他選項.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learing
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You ordered " + 2 + "items at $" + 3 + "each.");
+            Console.WriteLine("10 / 3 is $" + 10.0 / 3.0);
+            Console.WriteLine("February has {0} or {1} days.", 28, 29);
+            Console.WriteLine("February has {0, 10} or {1, 5} days.", 28, 29);
+            Console.WriteLine("10 / 3 is ${0:#.##}", 10.0 / 3.0);
+            Console.WriteLine("you account is {0:##,####.###}", 123456.789);
+            Console.WriteLine("you account is {0:#}", 123456.789);
+            Console.WriteLine("you account is {0:##}", 123456.789); //same as above
+            Console.WriteLine("you account is {0:##.#}", 123456.789); //會根據取到的位子最後沒取到的做四捨五入進位
+            Console.WriteLine("you account is {0:C}", 123456.789); //貨幣格式
+        }
+    }
+}
---
+++ C#設計實務/Ch-3型別_字面字_變數/輸出選項/預留與跳脫差別.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learing
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a table");
+            Console.WriteLine("num\tsquare\tcube");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{0}\t{1}\t{2}", i, i * i, i * i * i);
+            }
+            Console.WriteLine("{0, 10}{1, 10}{2, 10}", "name", "square", "cube");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0, 10}{1, 10}{2, 10}", i, i * i, i * i * i);
+            }
+            Console.WriteLine("This is the end of the table");
+        }
+    }
+}
