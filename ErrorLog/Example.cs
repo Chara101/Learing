36bded1a1feef1adfd02e016a126441c5dcc960e
chara001-admin <molagon94@gmail.com>
2025-03-28 20:54:59 +0800

    problem on nlog using

---
+++ ErrorLog/Example.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NLog;
+
+namespace Learing
+{
+    public static class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void Main()
+        {
+            try
+            {
+                Logger.Info("Hello world");
+                System.Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Goodbye cruel world");
+            }
+        }
+    }
+}
