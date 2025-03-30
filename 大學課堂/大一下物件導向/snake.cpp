1848bca54b61c0ef19d030a47c5d41fdbfd39162
chara001-admin <molagon94@gmail.com>
2025-03-30 14:27:40 +0800

    snake v0.3 add move

--- 大學課堂/大一下物件導向/snake.cpp
+++ 大學課堂/大一下物件導向/snake.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h>
 using namespace std;
 
-class Role{
+class Role{ // 角色基底
 protected:
     string name;
     char icon;
@@ -26,19 +28,27 @@ public:
     char GetIcon(){ return icon; };
 };
 
-class Snake : public Role{
+class Snake : public Role{ // 蛇
+private:
+    char face; // 蛇的方向
+    int rate; //蛇的速度
 public:
-    Snake() : Role("snake", 'S', 0, 0) {
+    Snake() {
+        Snake(0, 0);
     }
-    Snake(string n, char i, int x, int y) : Role(n, i, x, y) {
+    Snake(int x, int y) : Role("snake", 'S', x, y) {
+        face = 'L';
+        rate = 1;
     }
+    char GetFace(){ return face; };
+    void SetFace(char f){ this->face = f; };
 };
 
-class Apple : public Role{
+class Apple : public Role{ // 蘋果
 public:
-    Apple() : Role("apple", 'S', 0, 0) {
+    Apple() : Role("apple", 'A', 0, 0) {
     }
-    Apple(int x, int y) : Role("apple", 'S', x, y) {
+    Apple(int x, int y) : Role("apple", 'A', x, y) {
     }
     void GotHit(){
         cout << name << ": got hit" << endl;
@@ -46,17 +56,71 @@ public:
 };
 
 class Map{
+    /*
+        v陣列紀錄地圖的使用狀況
+        snakes紀錄蛇的整個身體
+    */
 private:
     int width;
     int height;
     Apple* apple;
-    vector<Role*> snakes;
+    deque<Snake*> snakes;
     vector<vector<int>> v;
+    COORD coord;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    void SetCursorPosition(int x, int y) {
+        coord.X = x;
+        coord.Y = y;
+        SetConsoleCursorPosition(hConsole, coord);
+    }
+    void HideCursor() {
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(hConsole, &cursorInfo);
+        cursorInfo.bVisible = false; // 隱藏游標
+        SetConsoleCursorInfo(hConsole, &cursorInfo);
+    }
+    void Move(Role &role, char face){ //改變值
+        int x = role.GetX();
+        int y = role.GetY();
+        switch(face){
+            case 'U':
+                if(y - 1 >= 0) role.SetY(role.GetY() + 1);
+                break;
+            case 'D':
+                if(y + 1 < height) role.SetY(role.GetY() + 1);
+                break;
+            case 'L':
+                if(x - 1 >= 0) role.SetX(role.GetX() - 1);
+                break;
+            case 'R':
+                if(x + 1 < width) role.SetX(role.GetX() + 1);
+                break;
+        }
+    }
+
+    void Move(Role role, char face, int &ox, int &oy){ //只取值不改變
+        int x = role.GetX();
+        int y = role.GetY();
+        switch(face){
+            case 'U':
+                if(y - 1 >= 0) y -= 1;
+                break;
+            case 'D':
+                if(y + 1 < height) y += 1;
+                break;
+            case 'L':
+                if(x - 1 >= 0) x -= 1;
+                break;
+            case 'R':
+                if(x + 1 < width) x += 1;
+                break;
+        }
+        ox = x;
+        oy = y;
+    }
 public:
     Map(){
-        width = 20;
-        height = 20;
-        Map(width, height);
+        Map(20, 20);
         // v = new vector<vector<int>>(height, vector<int>(width, 0));
         // apple = SetPos(new Apple(0, 0));
     }
@@ -64,6 +128,23 @@ public:
         v = vector<vector<int> >(height, vector<int>(width, 0));
         apple = new Apple();
         SetPos(*apple);
+        snakes.push_back(new Snake(0, 0));
+        SetPos(*snakes[0]);
+        int tempx;
+        int tempy;
+        Move(*snakes[0], 'R', tempx, tempy); //測試用
+        snakes.push_back(new Snake(tempx, tempy)); //測試用
+        Move(*snakes[0], 'R', tempx, tempy); //測試用
+        snakes.push_back(new Snake(tempx, tempy)); //測試用
+        HideCursor();
+    }
+
+    ~Map(){
+        delete apple;
+        for(auto temp : snakes){
+            delete temp;
+        }
+        //在偵錯時聽取copilot的建議，將記憶體釋放
     }
 
     void SetPos(Role &role){
@@ -83,11 +164,27 @@ public:
     }
 
     void Print(){
-        
+        system("cls");
+        SetCursorPosition(apple->GetX(), apple->GetY());
+        cout << apple->GetIcon();
+        for(auto temp : snakes){
+            SetCursorPosition(temp->GetX(), temp->GetY());
+            cout << temp->GetIcon();
+        }
+        int tempx, tempy;
+        Move(*snakes[0], snakes[0]->GetFace(), tempx, tempy); //測試用
+        snakes.push_front(new Snake(tempx, tempy));
+        snakes.pop_back();
     }
 };
 
 int main(){
     srand(time(NULL));
+    Map* map = new Map(20, 20);
+    while(!kbhit()){
+        map->Print();
+        Sleep(1000);
+    }
+    cin.get();
     return 0;
 }
