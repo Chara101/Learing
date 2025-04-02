58ebf6a4e1a9b19dcdda51083a41147928c3f065
chara001-admin <molagon94@gmail.com>
2025-04-02 11:01:14 +0800

    增加buttom

---
+++ Python/搶票機器人/test.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Chara Bot")
+window.geometry("400x300")
+window.resizable(False, False)
+test = tk.Button(window, text="Test", command=lambda: print("Test button clicked!"))
+test.pack(side = tk.BOTTOM)
+try:
+    window.mainloop()
+except:
+    print("Error: Mainloop failed to run.")3
+
+
+
+
+    
