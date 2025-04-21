4df1ec0cf81b975680821eb2675a2e71d5da5249
chara001-admin <molagon94@gmail.com>
2025-04-21 22:11:55 +0800

    執行時期建構

---
+++ C#設計實務/Ch-17執行時期型態識別_反映與屬性/反映/Constructor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NLog;
+
+namespace Learing
+{
+    public class MyClass
+    {
+        private int mine = 0;
+        public int MyProperty { get; set; }
+        public MyClass()
+        {
+            mine = 0;
+        }
+        public MyClass(int mine)
+        {
+            this.mine = mine;
+        }
+        public string MyMethod(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        public static string MyMethod3(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+    }
+    public static class Program
+    {
+
+        public static void Main()
+        {
+            Type t = typeof(MyClass);
+            ConstructorInfo[]? constructor = t.GetConstructors();
+            MyClass myClass;
+            int x = 10;
+            foreach (var temp in constructor)
+            {
+                ParameterInfo[] p = temp.GetParameters();
+                if(p.Length == 1 && p[0].ParameterType == typeof(String))
+                {
+                    myClass = new MyClass(x);
+                }
+            }
+        }
+    }
+}
