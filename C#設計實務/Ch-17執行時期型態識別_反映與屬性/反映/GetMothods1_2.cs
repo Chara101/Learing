398e8c50642e68083a18020c238d4fca69a3e43f
chara001-admin <molagon94@gmail.com>
2025-04-21 21:05:17 +0800

    獲得方法

---
+++ C#設計實務/Ch-17執行時期型態識別_反映與屬性/反映/GetMothods1_2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NLog;
+
+namespace Learing
+{
+    public class MyClass
+    {
+        private int mine = 0;
+        public int MyProperty { get; set; }
+        public string MyMethod(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+    }
+    public static class Program
+    {
+
+        public static void Main()
+        {
+            Type t = typeof(MyClass);
+            PropertyInfo[] properties = t.GetProperties();
+            MethodInfo[] methods = t.GetMethods();
+            foreach(PropertyInfo pi in properties)
+            {
+                Console.WriteLine(pi.PropertyType + " " + pi.Name);
+            }
+            foreach(MethodInfo mi in methods)
+            {
+                Console.Write(mi.ReturnType + " " + mi.Name + "(");
+                ParameterInfo[] parameters = mi.GetParameters();
+                for(int i = 0; i < parameters.Length; i++)
+                {
+                    if (i > 0)
+                    {
+                        Console.Write(", ");
+                    }
+                    Console.Write(parameters[i].ParameterType + " " + parameters[i].Name);
+                }
+                Console.WriteLine(")");
+            }
+        }
+    }
+}
---
+++ C#設計實務/Ch-17執行時期型態識別_反映與屬性/反映/GetMthod2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NLog;
+
+namespace Learing
+{
+    public class MyClass
+    {
+        private int mine = 0;
+        public int MyProperty { get; set; }
+        public string MyMethod(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+        private string MyMethod2(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        public static string MyMethod3(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+    }
+    public static class Program
+    {
+
+        public static void Main()
+        {
+            Type t = typeof(MyClass);
+            PropertyInfo[] properties = t.GetProperties();
+            MethodInfo[] methods = t.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+            foreach (var item in methods)
+            {
+                Console.Write(item.ReturnType + " " + item.Name + "(");
+                ParameterInfo[] parameters = item.GetParameters();
+                foreach (var parameter in parameters)
+                {
+                    Console.Write(parameter.ParameterType + " " + parameter.Name);
+                }
+                Console.WriteLine(")");
+
+            }
+        }
+    }
+}
