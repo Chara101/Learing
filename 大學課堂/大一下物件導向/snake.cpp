26d2ff0e7e80f065b9fbbda4e6ffdfac2f16f914
chara001-admin <molagon94@gmail.com>
2025-05-02 08:44:26 +0800

    邊框待修正

--- 大學課堂/大一下物件導向/snake.cpp
+++ 大學課堂/大一下物件導向/snake.cpp
@@ -9,18 +9,21 @@ protected:
     char icon;
     int x;
     int y;
+    int id;
 public:
     Role(){
         name = "role";
         icon = 'R';
         x = 0;
         y = 0;
+        id = 0;
     }
-    Role(string n, char i, int x, int y) : name(n), icon(i), x(x), y(y) {
+    Role(string n, char i, int x, int y, int d) : name(n), icon(i), x(x), y(y), id(d) {
     }
 
     int GetX(){ return this->x; };
     int GetY(){ return this->y; };
+    int GetId(){ return this->id; };
     void SetX(int x){ this->x = x; };
     void SetY(int y){ this->y = y; };
 
@@ -32,10 +35,11 @@ class Snake : public Role{ // 蛇
 private:
     char face; // 蛇的方向
     int rate; //蛇的速度
+    int id = 1;
 public:
     Snake() : Snake(0, 0, 'L') {
     }
-    Snake(int x, int y, char f) : Role("snake", 'S', x, y) {
+    Snake(int x, int y, char f) : Role("snake", 'S', x, y, 1) {
         face = f;
         rate = 1;
     }
@@ -45,9 +49,9 @@ public:
 
 class Apple : public Role{ // 蘋果
 public:
-    Apple() : Role("apple", 'A', 0, 0) {
+    Apple() : Role("apple", 'A', 0, 0, 2) {
     }
-    Apple(int x, int y) : Role("apple", 'A', x, y) {
+    Apple(int x, int y) : Role("apple", 'A', x, y, 2) {
     }
     void GotHit(){
         cout << name << ": got hit" << endl;
@@ -64,12 +68,12 @@ private:
     int height;
     Apple* apple;
     deque<Snake*> snakes;
-    vector<vector<int>> v;
+    vector<vector<int>> v; //紀錄地圖的使用狀況 0空 1蛇 2蘋果
     COORD coord;
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     void SetCursorPosition(int x, int y) {
-        coord.X = x;
-        coord.Y = y;
+        coord.X = x + 1;
+        coord.Y = y + 1; // +1是因為邊框的關係
         SetConsoleCursorPosition(hConsole, coord);
     }
     void HideCursor() {
@@ -79,7 +83,8 @@ private:
         SetConsoleCursorInfo(hConsole, &cursorInfo);
     }
 
-    void Move(Role role, char face, int &ox, int &oy){ //根據蛇頭的方向改變座標不改變蛇頭
+    int Move(Snake role, char face, int &ox, int &oy){ //根據蛇頭的方向改變座標不改變蛇頭
+        int status = 0; //紀錄蛇的狀態 0:正常 1:撞牆 2:撞到自己 3:吃到蘋果
         int x = role.GetX();
         int y = role.GetY();
         switch(face){ //根據方向改變座標
@@ -96,13 +101,42 @@ private:
                 if(x + 1 < width) x += 1;
                 break;
         }
+        v[y][x] = role.GetId(); //紀錄地圖的使用狀況
         ox = x; //傳回新的座標
         oy = y; //傳回新的座標
+        return status; //傳回狀態
+    }
+
+    void PrintFrame(){ //印出邊框
+        for(int i = 0; i < width + 2; i++){ //頂邊
+            SetCursorPosition(i, 0);
+            cout << '#';
+        }
+        for(int i = 0; i < height + 2; i++){ //側邊
+            SetCursorPosition(0, i);
+            cout << '#';
+            SetCursorPosition(width + 1, i);
+            cout << '#';
+        }
+        SetCursorPosition(0, height + 1);
+        for(int i = 0; i < width + 2; i++){
+            cout << '#';
+        }
+
+    }
+    void SetPos(Role &role){
+        int x = rand() % width;
+        int y = rand() % height;
+        while(v[y][x] != 0){
+            x = rand() % width;
+            y = rand() % height;
+        }
+        v[y][x] = role.GetId(); //紀錄地圖的使用狀況
+        role.SetX(x);
+        role.SetY(y);
     }
 public:
     Map() : Map(20, 20){
-        // v = new vector<vector<int>>(height, vector<int>(width, 0));
-        // apple = SetPos(new Apple(0, 0));
     }
     Map(int w, int h) : width(w), height(h) {
         HideCursor();
@@ -111,14 +145,11 @@ public:
         SetPos(*apple);
         snakes.push_back(new Snake());
         SetPos(*snakes[0]);
-        // int tempx;
-        // int tempy;
-        // Move(*snakes[0], 'R', tempx, tempy); //測試用
-        // snakes.push_back(new Snake(tempx, tempy)); //測試用
-        // Move(*snakes[0], 'R', tempx, tempy); //測試用
-        // snakes.push_back(new Snake(tempx, tempy)); //測試用
+        snakes.push_back(new Snake(*snakes[0]));
+        snakes.push_back(new Snake(*snakes[0]));
         SetCursorPosition(apple->GetX(), apple->GetY()); //設定蘋果位置
         cout << apple->GetIcon();
+        PrintFrame(); //印出邊框
     }
 
     ~Map(){
@@ -129,30 +160,18 @@ public:
         //在偵錯時聽取copilot的建議，將記憶體釋放
     }
 
-    void SetPos(Role &role){
-        int x = rand() % width;
-        int y = rand() % height;
-        while(v[y][x] != 0){
-            x = rand() % width;
-            y = rand() % height;
-        }
-        v[y][x] = 1;
-        role.SetX(x);
-        role.SetY(y);
-    }
-
     void Control(char keycode){
         switch(keycode){
-            case 72: // ↑
+            case 72: //上
                 snakes.front()->SetFace('U');
                 break;
-            case 80: // ↓
+            case 80: //下
                 snakes.front()->SetFace('D');
                 break;
-            case 75: // ←
+            case 75: //左
                 snakes.front()->SetFace('L');
                 break;
-            case 77: // →
+            case 77: //右
                 snakes.front()->SetFace('R');
                 break;
             default:
